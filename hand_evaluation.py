c804dcdb58d5c6013f9ca67aff179b366ab78c57
Ryan Goodwin <ryangoodwin0818@gmail.com>
2024-06-02 13:49:33 -0500

    initial commit

---
+++ hand_evaluation.py
@@ -0,0 +1,22 @@
+import input_handling
+
+def card_value(rank):
+    if rank in "23456789":
+        return int(rank)
+    elif rank in "TJQK":
+        return 10
+    elif rank == "A":
+        return 11
+    return 0
+
+def hand_evaluation(hand):
+    score = 0
+    num_aces = 0
+    for card in hand:
+        score += card_value(card.rank)
+        if card.rank == "A":
+            num_aces += 1
+    while score > 21 and num_aces > 0:
+        score -= 10
+        num_aces -= 1
+    return score
---
+++ input_handling.py
@@ -0,0 +1,19 @@
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+def dealer_input():
+    dealer_rank = input("Enter the dealer's card rank: ")
+    dealer_suit = input("Enter the dealer card suit: ")
+    dealer_card = Card(dealer_rank, dealer_suit)
+    return dealer_card
+
+def player_input():
+    hand = []
+    num_cards = int(input("Enter the amount of cards in your hand: "))
+    for i in range(num_cards):
+        card_rank = input(f"Enter the rank of card {i}: ")
+        card_suit = input(f"Enter the suit of card {i}: ")
+        hand.append(Card(card_rank, card_suit))
+    return hand
