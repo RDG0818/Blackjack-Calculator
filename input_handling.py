a571af6e2486b5977361a7879f8fbf37486496d7
Ryan Goodwin <ryangoodwin0818@gmail.com>
2024-06-02 14:33:51 -0500

    Added probability module and chance_to_bust func

--- input_handling.py
+++ input_handling.py
@@ -10,7 +10,7 @@ def player_input():
     hand = []
     num_cards = int(input("Enter the amount of cards in your hand: "))
     for i in range(num_cards):
-        card_rank = input(f"Enter the rank of card {i}: ")
-        card_suit = input(f"Enter the suit of card {i}: ")
+        card_rank = input(f"Enter the rank of card {i + 1}: ")
+        card_suit = input(f"Enter the suit of card {i + 1}: ")
         hand.append(Card(card_rank, card_suit))
     return hand
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from input_handling import *
 from hand_evaluation import *
+from probability import *
 dealer_card = dealer_input()
 player_hand = player_input()
-
-score = hand_evaluation(player_hand)
+print("Chance to bust is", str(chance_of_bust(dealer_card, player_hand)))
